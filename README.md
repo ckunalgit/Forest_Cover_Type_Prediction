9878133dd932d635575e2200ef4664c876466a01
Kunal Chowdhury <ckunalsinbox@gmail.com>
2022-07-17 18:37:21 +0530

    Updated initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Forest_Cover_Type_Prediction
 This model is helpful in noticing the changes occurred due to heavy floods or any other calamities which affected the forest land
+
+# Creating virtual env for project
+conda create -p venv_ffcp python=3.7 -y
+
+# Activating conda env
+conda activate E:\ineuron\vs\projects\Forest_Cover_Type_Prediction\venv_ffcp
+
+# Install required libraries {Before running this, created setup.py file}
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup,find_packages
+from typing import List
+
+
+#Declaring variables for setup functions
+PROJECT_NAME="forest_cover_type_predictor"
+VERSION="0.0.1"
+AUTHOR="Kunal Chowdhury"
+DESRCIPTION="This is forest cover predictor ML project"
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+
+setup(
+    name = PROJECT_NAME,
+    version = VERSION,
+    author = AUTHOR,
+    description = DESRCIPTION,
+
+# This find_packages() basically scans the root folder for all folders having a __init__ file and returns it. We can hardode as "housing" as well
+    packages = find_packages(), #["housing"]
+    install_requires = get_requirements_list()
+    )
