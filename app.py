bb3929fc229184b8ae6a1579d39931b5c211966c
Kunal Chowdhury <ckunalsinbox@gmail.com>
2022-07-17 19:55:40 +0530

    Added Exception module

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from parent.logger import logging
+from parent.exception import ForestException
+import sys
 
 
 app = Flask(__name__)
@@ -7,8 +9,16 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     # Testing logging module
-    logging.info("We are testing logger module")
+    #logging.info("We are testing logger module")
+
+    # Testing exception
+    try :
+        raise Exception("Testing custom exception")
+    except Exception as e:
+        forest = ForestException(e,sys)
+        logging.info(forest.error_message)
     return "Starting forest cover project"
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
